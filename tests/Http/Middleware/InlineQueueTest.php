a4085516751f3beefc1bc5c13854572fa17b89c7
Chris Wright <chriswright@live.com>
2024-08-31 15:31:10 -0400

    Added tests

---
+++ tests/Http/Middleware/InlineQueueTest.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace Jorbascrumps\QueueIt\Test\Http\Middleware;
+
+use Illuminate\Support\Facades\Config;
+use Jorbascrumps\QueueIt\Http\Middleware\InlineQueue;
+use Jorbascrumps\QueueIt\Test\TestCase;
+use QueueIT\KnownUserV3\SDK\ActionTypes;
+use QueueIT\KnownUserV3\SDK\RequestValidationResult;
+
+class InlineQueueTest extends TestCase
+{
+    protected function defineWebRoutes($router): void
+    {
+        $subject = InlineQueue::eventId('test')->queueDomain(self::QUEUE_URL);
+
+        $router->middleware($subject)->get(self::PAGE_URL, fn () => 'Page content');
+    }
+
+    public function testFeatureDisabled(): void
+    {
+        Config::set('queue-it.enabled', false);
+
+        $response = $this->get(self::PAGE_URL);
+
+        $response->assertOk();
+    }
+
+    public function testPerformsQueueRedirect(): void
+    {
+        $userInQueueService = $this->mockQueueService();
+        $userInQueueService->validateQueueRequestResult = new RequestValidationResult(
+            ActionTypes::QueueAction, null, null, self::QUEUE_URL, null, null
+        );
+
+        $response = $this->get(self::PAGE_URL);
+
+        $response->assertRedirect(self::QUEUE_URL);
+    }
+
+    public function testTest2(): void
+    {
+        $userInQueueService = $this->mockQueueService();
+        $userInQueueService->validateQueueRequestResult = new RequestValidationResult(
+            ActionTypes::QueueAction, null, null, null, null, null
+        );
+
+        $response = $this->get(self::PAGE_URL . '?queueittoken=token');
+
+        $response->assertRedirect(self::PAGE_URL);
+    }
+
+    public function testIgnoresInvalidAction(): void
+    {
+        $userInQueueService = $this->mockQueueService();
+        $userInQueueService->validateQueueRequestResult = new RequestValidationResult(
+            ActionTypes::IgnoreAction, null, null, null, null, null
+        );
+
+        $response = $this->get(self::PAGE_URL . '?queueittoken=token');
+
+        $response->assertOk();
+    }
+
+    /**
+     * @dataProvider aliasProvider
+     */
+    public function testAlias($expected, $actual): void
+    {
+        $this->assertSame($expected, (string) $actual);
+    }
+
+    public static function aliasProvider(): array
+    {
+        return [
+            'default params' => [
+                'queue-it.inline-queue:,,,15,1,,',
+                new InlineQueue,
+            ],
+            'custom params' => [
+                'queue-it.inline-queue:eventId,queueDomain,cookieDomain,10,,culture,layoutName',
+                new InlineQueue('eventId', 'queueDomain', 'cookieDomain', 10, false, 'culture', 'layoutName'),
+            ],
+        ];
+    }
+}
